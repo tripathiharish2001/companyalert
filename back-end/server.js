a4719d9c68e58f148327d15675f8926cfbc24467
tripathiharish2001 <tripathiharish2001@gmail.com>
2023-08-24 03:31:38 +0530

    new commit

--- back-end/server.js
+++ back-end/server.js
@@ -8,6 +8,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
 app.use("/api/tasks", tasksRoutes);
 app.use("/api/user", userRoutes);
 
